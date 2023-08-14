255320c9ea9a10fe46eaf6e5342daaefd66ab524
denkisil <nedrocker227@gmail.com>
2023-08-14 23:09:36 +0300

    locales fixures

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,5 +1,14 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import I18NextHttpBackend from "i18next-http-backend";
+import path from "path";
 
-i18next.use(I18NextHttpBackend).use(initReactI18next).init({ lng: "en" });
+i18next
+  .use(I18NextHttpBackend)
+  .use(initReactI18next)
+  .init({
+    lng: "en",
+    backend: {
+      locationPath: path.resolve("locals/{{lng}}/translation.json")
+    }
+  });
